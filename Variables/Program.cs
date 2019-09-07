de7d73fa78548a2b3de23e311e77fcdf10ac2d90
Williano <paawilly17@gmail.com>
2019-09-07 11:36:20 +0000

    [UPDATE] :zap: Add documentation

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,6 +6,33 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            /*
+                Data Types:
+                byte, int, float, char, string, bool
+
+                Note:
+                    When intializing float or decimal, add
+                    'f' or 'm' to them respectively.
+                    Eg:
+                        float price = 2.2f;
+                        decimal pay = 2.4m;
+
+                We declare constant  with the 'Const' keyword.
+                Eg:
+                    const float PRICE = 3.4f;
+             */
+
+            /*
+             Variable Naming:
+               Variable Names should be camelCase.
+               Eg:
+                   float totalPrice = 20.5f;
+
+             Methods and Class names should be PascalCase.
+             Eg:
+                   class StudentInfo() {};
+                   public int GetStudentInfo() {};
+            */
             byte number = 3;
             int count = 10;
             float totalPrice = 20.95f;
