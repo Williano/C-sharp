b261adf7d9bdf98a8f486defe01eafb52162da98
Williano <paawilly17@gmail.com>
2019-09-12 12:42:05 +0000

    [ADD CODE] :white_check_mark: Add code for operators

---
+++ Operators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            var c = 4;
+
+            // Addition
+            var add = a + b;
+
+            // Division
+            var div = a / b;
+
+            // Not operator
+            var notOp = !(a != b);
+
+            // Equal to
+            var equal = (a == b);
+
+            // And operator
+            var andOp = c > b && a > c;
+
+            // Or operator
+            var orOp = c > b || a > c;
+
+            Console.WriteLine(add);
+            Console.WriteLine(div);
+            Console.WriteLine(notOp);
+            Console.WriteLine(equal);
+            Console.WriteLine(andOp);
+            Console.WriteLine(orOp);
+        }
+    }
+}
